14d947bc09e7fc7d2423d30e43034dd09fd37874
Kid Max <kidmax285@gmail.com>
2026-05-02 17:01:20 +0600

    refactor(address): simplify authorization logic in AddressOldController

--- app/Http/Controllers/AddressOldController.php
+++ app/Http/Controllers/AddressOldController.php
@@ -5,25 +5,22 @@ declare(strict_types=1);
 namespace Modules\Address\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Routing\Attributes\Controllers\Authorize;
+use Illuminate\Routing\Attributes\Controllers\Middleware;
 use Modules\Address\Http\Requests\AddressRequest;
 use Modules\Address\Models\Address;
 use Modules\Address\Resources\AddressResource;
 
+#[Middleware('auth:sanctum')]
 class AddressOldController extends Controller
 {
-    use AuthorizesRequests;
-
-    /**
-     * Display a listing of the resource.
-     */
+    #[Authorize('viewAny', Address::class)]
     public function index(Request $request): JsonResponse
     {
-        $user = $request->user();
-        $addresses = $user->addresses;
+        $addresses = $request->user()->addresses;
 
         return response()->json([
             'message' => 'Success',
@@ -31,18 +28,10 @@ class AddressOldController extends Controller
         ], Response::HTTP_OK);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    #[Authorize('create', Address::class)]
     public function store(AddressRequest $request): JsonResponse
     {
-        $user = $request->user();
-
-        // Authorize the user to create an address
-        $this->authorize('create', Address::class);
-
-        // Create the address using validated data
-        $address = $user->addresses()->create($request->validated());
+        $address = $request->user()->addresses()->create($request->validated());
 
         return response()->json([
             'message' => 'Address created successfully',
@@ -50,29 +39,18 @@ class AddressOldController extends Controller
         ], Response::HTTP_CREATED);
     }
 
-    /**
-     * Show the specified resource.
-     */
+    #[Authorize('view', Address::class)]
     public function show(Address $address): JsonResponse
     {
-        // Authorize the user to view the address
-        $this->authorize('view', $address);
-
         return response()->json([
             'message' => 'Success',
             'address' => new AddressResource($address),
         ], Response::HTTP_OK);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    #[Authorize('update', Address::class)]
     public function update(AddressRequest $request, Address $address): JsonResponse
     {
-        // Authorize the user to update the address
-        $this->authorize('update', $address);
-
-        // Update the address using validated data
         $address->update($request->validated());
 
         return response()->json([
@@ -81,14 +59,9 @@ class AddressOldController extends Controller
         ], Response::HTTP_OK);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    #[Authorize('delete', Address::class)]
     public function destroy(Address $address): JsonResponse
     {
-        // Authorize the user to delete the address
-        $this->authorize('delete', $address);
-
         $address->delete();
 
         return response()->json([
